215928275e1406569045bf061e4aba39257de078
Ivan Kapustian <michiro2845@gmail.com>
2023-08-03 13:23:44 +0300

    fix

---
+++ src/cat-api.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_5A6itroODULjMiDobyaN0GuwsF9jzUP43Mg5Jj7gViufoQ6YICdA8NTrXWdZRq94";
+
+fetchBreeds()
+fetchCatByBreed(breedId)
